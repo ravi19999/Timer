5996d4183ae016881a2973fa1dd28a66e41f72e3
ravi19999 <dahalravindra@gmail.com>
2020-06-16 15:15:39 +0545

    Added the start, pause and tick methods

--- index.js
+++ index.js
@@ -5,14 +5,32 @@ class Timer {
         this.pauseButton = pauseButton;
 
         this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
+    };
+
+    start = () => {
+        this.tick();
+        this.interval = setInterval(this.tick, 1000);
     }
 
-    start() {
-        console.log('Time to start the timer');
+    pause = () => {
+        clearInterval(this.interval);
+    };
+
+    tick = () => {
+        this.timeRemaining = this.timeRemaining - 1;
+    };
+
+    get timeRemaining() {
+        return parseFloat(this.durationInput.value);
+    }
+
+    set timeRemaining(time) {
+        this.durationInput.value = time;
     }
 }
 
-const durationInput = document.querySelector('#input');
+const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
