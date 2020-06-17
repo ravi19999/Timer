ca8d991f6b9e231190c5b256ed6e63c2edc1c722
ravi19999 <dahalravindra@gmail.com>
2020-06-17 07:37:18 +0545

    Added css elements, modified the border such that it empties out as per the entered time

--- index.js
+++ index.js
@@ -2,13 +2,19 @@
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('circle');
 
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let duration;
 const timer = new Timer(durationInput, startButton, pauseButton, {
-    onStart() {
-        console.log('Timer started');
+    onStart(totalDuration) {
+        duration = totalDuration;
     },
-    onTick() {
-        console.log('Ticking')
+    onTick(timeRemaining) {
+        circle.setAttribute('stroke-dashoffset',
+            perimeter * timeRemaining / duration - perimeter);
     },
     onComplete() {
         console.log("And we're done");
